24c090435999a1d989830988050f830e32624d1a
Lavani Mujaheed Ali Khan <162157594+MujaheedAliKhan@users.noreply.github.com>
2026-04-03 15:37:29 +0530

    Added some changes

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import {motion} from 'framer-motion'
+import {motion} from 'framer-motion' //Transition
+import {TypeAnimation} from 'react-type-animation' //Text Animation
 const Aboutme = () => {
 
 
@@ -13,7 +14,19 @@ const Aboutme = () => {
           transition={{duration: 1}}
           className='mx-auto mb-6 h-72 w-72 object-cover border-gray-400 border-2 rounded-full shadow-gray-400 shadow-md ' src="/images/Mujaheed.jpeg" alt="Mujaheed.jpeg" />
           <h1 className='text-4xl mb-4 text-center font-bold text-gray-500 text-wrap'><span className='text-black'>I'm</span> Lavani Mujaheed Ali Khan, </h1>
-          <h2 className='text-3xl mb-4 font-bold'><span className='text-black'>Frontend Developer</span></h2>
+          <h2 className='text-3xl mb-4 font-bold'><span className='text-black'>
+            <TypeAnimation
+            sequence={[
+              "Frontend Developer", 2000,
+              "JavaScript Enthusiast", 2000,
+              "Responsive Web Designer", 2000,
+              "React Developer"
+            ]}
+            speed={10}
+            repeat={Infinity}>
+
+            </TypeAnimation>
+            </span></h2>
           <p className='max-w-5xl text-wrap p-4 text-md text-center sm:text-md font-semibold text-gray-600 '>Enthusiastic and dedicated fresher seeking an opportunity as an Analyst Trainee in the IT sector
               to provide technical support, troubleshoot hardware and software issues, and deliver excellent
               customer service while contributing to organizational growth. Quick learner with strong
@@ -26,12 +39,12 @@ const Aboutme = () => {
           initial={{opacity:0, x:-30}}
           animate={{opacity:1, x:0}}
           transition={{duration:0.8}}
-          href="../src/assets/images/LavaniMujaheedAliKhan_resume.pdf" className='border-none text-md font-bold bg-black text-white px-5 py-3 rounded-full transition duration-200 shadow-lg hover:bg-gray-700' target='_blank'>View Resume</motion.a>
+          href="../public/images/Mujaheed_Ali_KhanRes.pdf" className='border-none text-md font-bold bg-black text-white px-5 py-3 rounded-full transition duration-200 shadow-lg hover:bg-gray-700' target='_blank'>View Resume</motion.a>
           <motion.a 
           initial={{opacity:0, x:30}}
           animate={{opacity:1, x:0}}
           transition={{duration:0.8}}
-          href="../src/assets/images/LavaniMujaheedAliKhan_resume.pdf" className='border-none text-md font-bold bg-gray-700 text-white px-5 py-3 rounded-full transition duration-150 shadow-lg  hover:bg-black' download={"LavaniMujaheedAliKhan_resume.pdf"}>Download CV</motion.a>
+          href="../public/images/Mujaheed_Ali_KhanRes.pdf" className='border-none text-md font-bold bg-gray-700 text-white px-5 py-3 rounded-full transition duration-150 shadow-lg  hover:bg-black' download={"LavaniMujaheedAliKhan_resume.pdf"}>Download CV</motion.a>
         </div>
         
       </div>

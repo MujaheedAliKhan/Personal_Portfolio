9d954f0cfa9f251a961f8243fe457eae6e772bb6
Lavani Mujaheed Ali Khan <162157594+MujaheedAliKhan@users.noreply.github.com>
2026-05-15 16:34:18 +0530

    Added-some-changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div id='projects'className='min-h-screen w-full pb-20'>
       <h1 className='text-center pb-8 text-5xl font-bold text-gray-700'>PROJECTS</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 items-stretch p-12 md:p-4 gap-10 w-full ">
+      <div className="grid grid-cols-1 md:grid-cols-3 items-stretch p-8 md:p-4 gap-10 w-full ">
         {data && data.map((item) => {
             return(
               <div className="pt-6 pb-8 p-4 md:p-6 flex flex-col justify-center items-center border border-gray-300 rounded-lg shadow-2xl bg-white hover:translate-x-1 hover:scale-105 duration-200 delay-100 ease-in-out" key={item.id}>
@@ -33,7 +33,7 @@ const Projects = () => {
                         );
                   })}
                  </div>
-                  <p className="pb-4 text-center text-gray-600 text-sm p-4">{item.description}</p>
+                  <p className="pb-4 text-center text-gray-600 text-xs md:text-sm p-4">{item.description}</p>
                   <a href={item.live} className="text-center px-14 py-3 text-xs md:text-sm font-bold flex justify-center items-center border border-none bg-gray-900 text-gray-100 rounded-full shadow-lg hover:bg-gray-800 transition duration-200 tracking-wider" target="_blank" rel="noopener noreferer">View <RxExternalLink  className="ml-2 text-lg"/></a>
               </div>
             )
